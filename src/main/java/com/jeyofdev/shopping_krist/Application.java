86dd8c106cc6f7f993d7431f0bbebea85153f536
jeyofdev <jgregoire.dev@gmail.com>
2024-12-29 18:10:45 +0100

    config : init database

--- src/main/java/com/jeyofdev/shopping_krist/Application.java
+++ src/main/java/com/jeyofdev/shopping_krist/Application.java
@@ -1,14 +1,26 @@
 package com.jeyofdev.shopping_krist;
 
+import com.jeyofdev.shopping_krist.config.DatabaseConfig;
+import com.jeyofdev.shopping_krist.util.DatabaseInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		DatabaseConfig config = context.getBean(DatabaseConfig.class);
 
-		SpringApplication.run(Application.class, args);
+		// Check if database exist
+		DatabaseInitializer.initializeDatabase(
+				"jdbc:postgresql://localhost:5432/postgres",
+				config.getDbUser(),
+				config.getDbPassword(),
+				config.getDbName()
+		);
+
 		System.out.println("hello world");
 	}
 
---
+++ src/main/java/com/jeyofdev/shopping_krist/util/DatabaseInitializer.java
@@ -0,0 +1,26 @@
+package com.jeyofdev.shopping_krist.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseInitializer {
+    public static void initializeDatabase(String jdbcUrl, String user, String password, String dbName) {
+        String createDbQuery = "CREATE DATABASE " + dbName;
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+             Statement statement = connection.createStatement()) {
+
+            // If the database does not exist, create it
+            statement.executeUpdate(createDbQuery);
+            System.out.println("Database created successfully!");
+        } catch (SQLException e) {
+            if (e.getSQLState().equals("42P04")) { // Code error for "Database already exists"
+                System.out.println("The database already exists.");
+            } else {
+                System.err.println("Error creating database : " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
