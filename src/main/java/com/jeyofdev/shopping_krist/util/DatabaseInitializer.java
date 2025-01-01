8a09372b532c8939e27c158060b9d687b9d340ea
jeyofdev <jgregoire.dev@gmail.com>
2025-01-01 16:52:01 +0100

    feat : init datas product, cart and cartItem in databaseInitalizer

--- src/main/java/com/jeyofdev/shopping_krist/util/DatabaseInitializer.java
+++ src/main/java/com/jeyofdev/shopping_krist/util/DatabaseInitializer.java
@@ -1,15 +1,50 @@
 package com.jeyofdev.shopping_krist.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
+import com.jeyofdev.shopping_krist.core.enums.Color;
+import com.jeyofdev.shopping_krist.core.enums.Size;
+import com.jeyofdev.shopping_krist.domain.cart.Cart;
+import com.jeyofdev.shopping_krist.domain.cart.CartMapper;
+import com.jeyofdev.shopping_krist.domain.cart.CartService;
+import com.jeyofdev.shopping_krist.domain.cart.dto.SaveCartDTO;
+import com.jeyofdev.shopping_krist.domain.cartItem.CartItem;
+import com.jeyofdev.shopping_krist.domain.cartItem.CartItemMapper;
+import com.jeyofdev.shopping_krist.domain.cartItem.CartItemService;
+import com.jeyofdev.shopping_krist.domain.cartItem.dto.SaveCartItemDTO;
+import com.jeyofdev.shopping_krist.domain.product.Product;
+import com.jeyofdev.shopping_krist.domain.product.ProductMapper;
+import com.jeyofdev.shopping_krist.domain.product.ProductService;
+import com.jeyofdev.shopping_krist.domain.product.dto.SaveProductDTO;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-public class DatabaseInitializer {
+import java.io.IOException;
+import java.sql.*;
+import java.text.MessageFormat;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseInitializer implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(DatabaseInitializer.class);
+
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+    private final CartService cartService;
+    private final CartMapper cartMapper;
+    private final CartItemService cartItemService;
+    private final CartItemMapper cartItemMapper;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Database initialization started...");
+        this.createDatas();
+    }
 
     public static void initializeDatabase(String jdbcUrl, String user, String password, String dbName) {
-        String createDbQuery = "CREATE DATABASE " + dbName;
+        String createDbQuery = MessageFormat.format("CREATE DATABASE {0}", dbName);
         try (Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
              Statement statement = connection.createStatement()) {
 
@@ -20,9 +55,61 @@ public class DatabaseInitializer {
             if (e.getSQLState().equals("42P04")) { // Code error for "Database already exists"
                 System.out.println("The database already exists.");
             } else {
-                System.err.println("Error creating database : " + e.getMessage());
+                System.err.println(MessageFormat.format("Error creating database : {0}", e.getMessage()));
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private void createDatas() throws IOException {
+        this.createProducts();
+       this.createCarts();
+       this.createCartItems();
+    }
+
+    private void createProducts() {
+        Product productA = productMapper.mapToEntity(new SaveProductDTO(
+                "adidas",
+                "men red t-shirt",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra ligula nec sapien imperdiet, eu mattis enim cursus. Donec tristique arcu eget dolor blandit, posuere porta mi rhoncus. Nulla non urna metus. Sed ullamcorper non quam id euismod. Pellentesque ac dolor et ante tincidunt dignissim. Ut cursus mi eu odio convallis, vitae accumsan ipsum luctus. Aenean massa sapien, pretium egestas sollicitudin nec, vulputate a diam. Vestibulum at lobortis urna.",
+                11.99,
+                15.99,
+                23,
+                Color.BLUE,
+                Size.L
+        ));
+
+        Product productB = productMapper.mapToEntity(new SaveProductDTO(
+                "nike",
+                "short white",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra ligula nec sapien imperdiet, eu mattis enim cursus. Donec tristique arcu eget dolor blandit, posuere porta mi rhoncus. Nulla non urna metus. Sed ullamcorper non quam id euismod. Pellentesque ac dolor et ante tincidunt dignissim. Ut cursus mi eu odio convallis, vitae accumsan ipsum luctus. Aenean massa sapien, pretium egestas sollicitudin nec, vulputate a diam. Vestibulum at lobortis urna.",
+                25.99,
+                35.99,
+                2,
+                Color.WHITE,
+                Size.XL
+        ));
+        productService.save(productA);
+        productService.save(productB);
+    }
+
+    private void createCarts() {
+        Cart cartA = cartMapper.mapToEntity(new SaveCartDTO());
+        Cart cartB = cartMapper.mapToEntity(new SaveCartDTO());
+
+        cartService.save(cartA);
+        cartService.save(cartB);
+    }
+
+    private void createCartItems() {
+        CartItem cartItemA = cartItemMapper.mapToEntity(new SaveCartItemDTO(5));
+        CartItem cartItemB = cartItemMapper.mapToEntity(new SaveCartItemDTO(2));
+
+        UUID firstProductId = productService.findAll().getFirst().getId();
+        UUID secondProductId = productService.findAll().get(1).getId();
+        UUID firstCartId = cartService.findAll().getFirst().getId();
+
+        cartItemService.save(cartItemA, firstProductId, firstCartId);
+        cartItemService.save(cartItemB, secondProductId, firstCartId);
     }
 }
